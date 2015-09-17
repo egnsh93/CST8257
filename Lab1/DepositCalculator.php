815a504dddaef86c60c9d38cea0f9ece6415e12c
Shane Egan <shane.egan93@gmail.com>
2015-09-16 23:07:00 -0400

    Initial commit

---
+++ Lab1/DepositCalculator.php
@@ -0,0 +1,95 @@
+<?php
+
+    // Extract the submitted data into variables
+    $principalAmount = $_POST['principalAmount'];
+    $interestRate = $_POST['interestRate'];
+    $depositDuration = $_POST['depositDuration'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $preferredContact = $_POST['preferredContact'];
+    $contactTime = $_POST['contactTimeChoice'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Deposit Calculator</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="css/style.css" rel="stylesheet">
+    <link href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <h2><strong>Thank you, <?= $name ?> for using our deposit calculator</strong></h2>
+        <p>You have entered the following data</p>
+        <table class="pure-table">
+            <tr class="pure-table-odd">
+                <td>Principal Amount:</td>
+                <td>
+                    <?= $principalAmount ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Interest Rate:</td>
+                <td>
+                    <?= $interestRate ?>
+                </td>
+            </tr>
+
+            <tr class="pure-table-odd">
+                <td>Deposit Duration:</td>
+                <td>
+                    <?= $depositDuration ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Name:</td>
+                <td>
+                    <?= $name ?>
+                </td>
+            </tr>
+
+            <tr class="pure-table-odd">
+                <td>Phone:</td>
+                <td>
+                    <?= $phone ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Email:</td>
+                <td>
+                    <?= $email ?>
+                </td>
+            </tr>
+
+            <tr class="pure-table-odd">
+                <td>Preferred Contact Method:</td>
+                <td>
+                    <?= $preferredContact ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>Preferred Contact Time:</td>
+                <td>
+                    <?= implode(", ", $_POST['contactTimeChoice']) ?>
+                </td>
+            </tr>
+        </table>
+
+        <p>Click <a href="index.html">here</a> to return to the following page</p>
+    </div>
+</body>
+
+</html>
