d6095e19ced40299f48d5435e96aad18a867e868
Shane Egan <shane.egan93@gmail.com>
2015-11-25 21:32:21 -0500

    Add navigation

--- Lab9/app/templates/default/header.php
+++ Lab9/app/templates/default/header.php
@@ -40,4 +40,36 @@ $hooks = Hooks::get();
 $hooks->run('afterBody');
 ?>
 
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/Lab9/Home">Online Course Registration</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="/Lab9/Courses/">Course Selection</a>
+                    </li>
+                    <li>
+                        <a href="/Lab9/RegisteredCourses/">Current Registration</a>
+                    </li>
+                    <li>
+                        <a href="/Lab9/About/">About</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
 <div class="container">
