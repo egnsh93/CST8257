569d7f8dfc81e2c15fc838ed0a2e1ad5e43a1327
Shane Egan <shane.egan93@gmail.com>
2015-11-25 21:34:35 -0500

    Add selection and listing views

---
+++ Lab9/app/views/course/selection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Course selection page
+ */
+
+use Core\Language;
+
+?>
+
+<div class="page-header">
+	<h1><?php echo $data['title'] ?></h1>
+</div>
+
+<p><?php echo $data['welcome_message'] ?></p>
