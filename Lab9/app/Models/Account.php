bc468fc86bb642501d21ce28d0822dee4fab4577
Shane Egan <shane.egan93@gmail.com>
2015-11-27 03:44:00 -0500

    Add Account model

---
+++ Lab9/app/Models/Account.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Models;
+
+use Core\Model;
+
+class Account extends Model 
+{    
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getStudentHash($id) {
+		return $this->db->select("SELECT StudentId, Name, Password FROM " . "student WHERE StudentId = :id",
+			array(
+				':id' => $id
+			));
+    }
+
+	public function insertStudent($student) {
+		$this->db->insert("student", $student);
+		return $this->db->lastInsertId('StudentId');
+	}
+}
